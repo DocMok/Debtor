fa041256a5131c75fffd4a94d7b9f82addc948bc
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2022-04-26 21:10:34 +0300

    views design changes

--- laravel/resources/views/user/show.blade.php
+++ laravel/resources/views/user/show.blade.php
@@ -8,39 +8,32 @@
         <div class="card-body">
             <div class="form-group">
                             <label for="name">Имя</label>
-                <div type="text" name="name" class="form-control" id="name" value="{{$user->name}}">
+                <div>
                     <p>{{$user->name}}</p>
                 </div>
                 <div class="form-group">
                     <label for="email">Почта</label>
-                    <div type="email" name="email" class="form-control" id="email" >
+                    <div>
                         <p>{{$user->email}}</p>
                     </div>
                     <div class="form-group">
                         <label for="phone">Номер телефона</label>
-                        <div type="text" name="phone" class="form-control" id="phone" >
+                        <div>
                             <p>{{$user->phone}}</p>
                         </div>
-                        <div class="form-group">
+                        <div>
                             <label for="organization">Название организации</label>
-                            <div type="text" name="organization" class="form-control" id="organization" >
+                            <div >
                                 <p>{{$user->organization}}</p>
                             </div>
 
-                    <div class="form-group">
-                        <label for="password">Пароль</label>
-                        <div type="password" name="password" class="form-control" id="password">
-                            <p></p>
-                        </div>
-                    </div>
-
-                    <div type='hidden' name="user_id" value="{{$user->id}}">
+                    <div>
                         <div class="card-footer row">
                             <form class=""  method=get
                                   action="{{route('user.edit', $user->id)}}" style="margin:2px;">
                                 @csrf
                                 <button class="btn btn-primary" type="submit">
-                                    Change
+                                    Изменить
                                 </button>
                                 <input type="hidden" name="user_id" value="{{$user->id}}">
                             </form>
